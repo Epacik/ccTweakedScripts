31723698878dd9291bc63b22e15fb8bc13a29000
epacik <epaciatko@gmail.com>
2020-12-02 19:29:13 +0100

    possible updater

---
+++ updates/getUpdater.lua
@@ -0,0 +1,20 @@
+print("installing installer")
+fs.delete("install")
+fs.delete("install.lua")
+shell.run("pastebin get eTD2puM7 install")
+
+
+print("installing uninstaller")
+fs.delete("uninstall")
+fs.delete("uninstall.lua")
+shell.run("pastebin get iJZbPYGQ uninstall")
+
+
+print("installing updater")
+fs.delete("update")
+fs.delete("update.lua")
+shell.run("pastebin get ZJ39anXN update")
+
+fs.delete("updateAll")
+fs.delete("updateAll.lua")
+shell.run("pastebin get KY6VE0NC updateAll")
