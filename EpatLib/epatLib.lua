ecd9036d50afc84fe75b91a924db49f0029459f2
epacik <epaciatko@gmail.com>
2020-12-28 01:40:40 +0100

    gateSrv1

--- EpatLib/epatLib.lua
+++ EpatLib/epatLib.lua
@@ -1,41 +1,30 @@
+local mcb = function() end
+local brkClbc 
 
 
-function string:split(sep)
-    local sep, fields = sep or ":", {}
-    local pattern = string.format("([^%s]+)", sep)
-    self:gsub(pattern, function(c) fields[#fields+1] = c end)
-    return fields
- end
+function SetMainLoopCallback(callback)
+    if(type(callback) == "function") then mcb = callback end
+end
+
+function SetBreakCallback(callback)
+    if(type(callback) == "function") then brkClbc = callback end
+end
+ 
+local function MainLoop()
+    while true do
+    
+        mcb()
+
+        os.queueEvent("randomEvent")
+        os.pullEvent()
 
 
- local mcb = function() end
- local brkClbc 
- 
- 
- function SetMainLoopCallback(callback)
-     if(type(callback) == "function") then mcb = callback end
- end
- 
- function SetBreakCallback(callback)
-     if(type(callback) == "function") then brkClbc = callback end
- end
- 
- 
- local function MainLoop()
-     while true do
-     
-         mcb()
- 
-         os.queueEvent("randomEvent")
-         os.pullEvent()
- 
- 
-         if brkClbc ~= nil and brkClbc() then
-             break;
-         end
-     end
- end
- 
- function Run()
-     MainLoop();
- end
+        if brkClbc ~= nil and brkClbc() then
+            break;
+        end
+    end
+end
+
+function Run()
+    MainLoop();
+end
--- gate/gateServer.lua
+++ gate/gateServer.lua
@@ -1,5 +1,43 @@
 os.loadAPI("/usr/apis/epatLib");
 elb = epatLib;
+os.loadAPI("/usr/apis/stringLib");
+
+local args = {...};
+
+local function newConfig(lines, linesNumber)
+    local conf = {};
+    for i = 1, linesNumber, 1 do
+        if(lines[i]:startsWith("IDs")) then
+            local ids = lines[i]:split(":");
+            if(ids[2] ~= nil) then
+                conf.IDs = ids[2]:split(";");
+            end
+        elseif (lines[i]:startsWith("pass")) then
+            conf.pass = lines[i]:split(":")[2]
+        end
+
+    end
+
+    return conf;
+end
+
+local function GetConfig()
+    local file = io.open("etc/gate.conf", "r");
+    if file == nil then
+        return nil;
+    end
+    local lines = {};
+    local index = 1;
+    while true do
+        local line = file:read();
+        if line == nil then
+            break
+        end
+        lines[index] = line; 
+        index = index + 1;
+    end
+    return newConfig(lines, index);
+end
 
 local function ConnectToNetwork()
 
@@ -15,13 +53,22 @@ local function ConnectToNetwork()
 end
 
 local function SetupHost()
-    
+    local host = "epGateHost";
+
+    if args[1] ~= nil then
+        host = args[1];
+    end
+
+    rednet.host("epGate", host)
 end
 
 elb.SetMainLoopCallback(function ()
     local sender, message, protocol = rednet.receive("gate");
+    local conf = GetConfig();
 end)
 
 ConnectToNetwork();
+
+SetupHost();
 
 elb.Run();
